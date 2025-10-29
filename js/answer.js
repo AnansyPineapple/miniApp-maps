93a74995ea9c296fb723274f3beda5396fd30e52
AnansyPineapple <anansy.pineapple@gmail.com>
2025-10-29 14:45:12 +0500

    16 Update answer.js
    svg for coords

--- js/answer.js
+++ js/answer.js
@@ -19,6 +19,18 @@ if (routeData && routeData.places) {
               <span>${place.address}</span>
             </div>
             <div class="path">
+              <svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14 8C14 11.3137 11.3137 14 8 14C4.68629 14 2 11.3137 2 8C2 4.68629 4.68629 2 8 2C11.3137 2 14 4.68629 14 8ZM2.99207 8C2.99207 10.7658 5.2342 13.0079 8 13.0079C10.7658 13.0079 13.0079 10.7658 13.0079 8C13.0079 5.2342 10.7658 2.99207 8 2.99207C5.2342 2.99207 2.99207 5.2342 2.99207 8Z" fill="#FF7B00"></path>
+                <path d="M11 8C11 9.65685 9.65685 11 8 11C6.34315 11 5 9.65685 5 8C5 6.34315 6.34315 5 8 5C9.65685 5 11 6.34315 11 8ZM5.9696 8C5.9696 9.12136 6.87864 10.0304 8 10.0304C9.12136 10.0304 10.0304 9.12136 10.0304 8C10.0304 6.87864 9.12136 5.9696 8 5.9696C6.87864 5.9696 5.9696 6.87864 5.9696 8Z" fill="#FF7B00"></path>
+                <line x1="2.66675" y1="8.22222" x2="8.12813e-05" y2="8.22222" stroke="#FF7B00" stroke-width="0.444444"></line>
+                <line x1="-1.29515e-08" y1="7.77778" x2="2.66667" y2="7.77778" stroke="#FF7B00" stroke-width="0.444444"></line>
+                <line x1="16" y1="8.22222" x2="13.3333" y2="8.22222" stroke="#FF7B00" stroke-width="0.444444"></line>
+                <line x1="13.3333" y1="7.77778" x2="15.9999" y2="7.77778" stroke="#FF7B00" stroke-width="0.444444"></line>
+                <line x1="7.77778" y1="2.66666" x2="7.77778" y2="-1.0252e-05" stroke="#FF7B00" stroke-width="0.444444"></line>
+                <line x1="8.22222" y1="-5.77347e-09" x2="8.22222" y2="2.66667" stroke="#FF7B00" stroke-width="0.444444"></line>
+                <line x1="7.77778" y1="16" x2="7.77778" y2="13.3333" stroke="#FF7B00" stroke-width="0.444444"></line>
+                <line x1="8.22222" y1="13.3333" x2="8.22222" y2="16" stroke="#FF7B00" stroke-width="0.444444"></line>
+              </svg>
               <a href="https://yandex.ru/maps/?pt=${place.coord[1]},${place.coord[0]}&z=16&l=map" target="_blank">
                 ${place.coord[1]}, ${place.coord[0]}
               </a>
