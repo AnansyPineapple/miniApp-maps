a903976961268b56302bdb53c58e775f3dd0b184
Maria_K <anansy.pineapple@gmail.com>
2025-10-29 12:38:24 +0500

    try 8

--- js/index.js
+++ js/index.js
@@ -39,7 +39,7 @@ for(let i = 1; i <= 5; i++){
 });
 
 document.getElementById('routeForm').addEventListener('submit', async (e) => {
-  e.preventDefault();
+  //e.preventDefault();
 
   const data = {
     query: firstTextarea.value,
