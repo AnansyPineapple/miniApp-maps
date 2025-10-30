fc7b532d8b40b1cf7c5182b3ff14e4c74687e858
Maria_K <anansy.pineapple@gmail.com>
2025-10-30 15:55:15 +0500

    карта 7

--- js/map.js
+++ js/map.js
@@ -12,62 +12,57 @@ function init() {
     const routeData = JSON.parse(localStorage.getItem('routeData'));
 
     if (routeData && routeData.places && Array.isArray(routeData.places)) {
-        const points = [];
-
         //Добавляем стартовую точку
         const start_point = ymaps.geocode(routeData.startPoint, {results: 1})
             .then(function (start) {
-                const coords = start.geoObjects.get(0).geometry.getCoordinates();
-                points.push(coords);
+                const start_coords = start.geoObjects.get(0).geometry.getCoordinates();
+
+                const points = [start_coords];
 
                 const start_placemark = new ymaps.Placemark(
-                    coords,
+                    start_coords,
                     {balloonContent: "Start Point"},
                     {preset: "islands#redDotIcon" }
                 );
                 map.geoObjects.add(start_placemark);
-            });
 
-        //Добавляем остальные точки
-        routeData.places.forEach((place, index) => {
-            const coords = [place.coord[0], place.coord[1]];
-            points.push(coords);
+                //Добавляем остальные точки
+                routeData.places.forEach((place, index) => {
+                    const coords = [place.coord[0], place.coord[1]];
+                    points.push(coords);
 
-            const placemark = new ymaps.Placemark(
-                coords,
-                { balloonContent: place.title },
-                { preset: "islands#orangeDotIcon" }
-            );        
-            map.geoObjects.add(placemark);
-        });
+                    const placemark = new ymaps.Placemark(
+                        coords,
+                        { balloonContent: place.title },
+                        { preset: "islands#orangeDotIcon" }
+                    );        
+                    map.geoObjects.add(placemark);
+                });
 
-        start_point.then(() => {
-            const multiRoute = new ymaps.multiRouter.MultiRoute({
-                referencePoints: points,
-            },{
-                editorDrawOver: false,
-                wayPointDraggable: true,
-                viaPointDraggable: true,
-                routeStrokeColor: "000088",
-                routeActiveStrokeColor: "ff0000",
-                pinIconFillColor: "ff0000",
-                boundsAutoApply: true,
-                zoomMargin: 30
-            });
+                const multiRoute = new ymaps.multiRouter.MultiRoute({
+                    referencePoints: points,
+                },{
+                    editorDrawOver: false,
+                    routeStrokeColor: "000088",
+                    routeActiveStrokeColor: "ff0000",
+                    pinIconFillColor: "ff0000",
+                    boundsAutoApply: true,
+                    zoomMargin: 30
+                });
 
-            map.geoObjects.add(multiRoute);
+                map.geoObjects.add(multiRoute);
         
-            multiRoute.events.once('update', function() {
-                const routes = multiRoute.getRoutes();
-                for (let i = 0, l = routes.getLength(); i < l; i++) {
-                    const route = routes.get(i);
-                    if (!route.properties.get('blocked')) {
-                        multiRoute.setActiveRoute(route);
-                        route.balloon.open();
-                        break;
+                multiRoute.events.once('update', function() {
+                    const routes = multiRoute.getRoutes();
+                    for (let i = 0, l = routes.getLength(); i < l; i++) {
+                        const route = routes.get(i);
+                        if (!route.properties.get('blocked')) {
+                            multiRoute.setActiveRoute(route);
+                            route.balloon.open();
+                            break;
+                        }
                     }
-                }
+                });
             });
-        });
     }
 }
