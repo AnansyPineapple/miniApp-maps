b4f33268f6d04447254497366921a531c8afaf7b
abchihba0 <113386115+abchihba0@users.noreply.github.com>
2025-10-31 03:32:14 +0300

    modified:   css/form.css 	modified:   js/suggsts.js

--- js/suggsts.js
+++ js/suggsts.js
@@ -1,11 +1,23 @@
 ymaps.ready(init);
 
 function init() {
-    new ymaps.SuggestView('thirdQuestion', {
+    const suggest = new ymaps.SuggestView('thirdQuestion', {
         boundedBy: [
-            [56.2, 43.8], // юго-запад
-            [56.5, 44.1]  // северо-восток
+            [56.2, 43.8],
+            [56.5, 44.1]
         ],
         strictBounds: true
+    });
+    
+    suggest.events.add('suggest', function(e) {
+        const data = e.get('suggestions');
+        const container = document.getElementById('suggestionsContainer');
+        container.innerHTML = '';
+        data.forEach(item => {
+            const div = document.createElement('div');
+            div.className = 'suggestion-item';
+            div.textContent = item.displayName;
+            container.appendChild(div);
+        });
     });
 }
